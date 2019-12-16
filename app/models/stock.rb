950be0815f7a88c3be5a67291289dab637dc91e1
bbborisk <bbborisk@gmail.com>
2019-12-16 15:25:47 +0100

    Made API auth a before action

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,9 @@
 class Stock < ApplicationRecord
   has_secure_password
 
-  def self.new_from_lookup(ticker_symbol)
       StockQuote::Stock.new(api_key: "sk_166dff3b7c4247c891e6f4418851bdbf")
+
+  def self.new_from_lookup(ticker_symbol)
       looked_up_stock =StockQuote::Stock.quote(ticker_symbol)
       new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
   end
