dca2033454059f01687fe85db56e5b22dfaf8640
bbborisk <bbborisk@gmail.com>
2019-12-17 13:54:40 +0100

    Fixed edge cases for stock search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,18 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
-  end
+    if params[:stock].present?
+        @stock = Stock.new_from_lookup(params[:stock])
+        if @stock
+          render 'users/my_portfolio'
+        else
+          flash[:danger] = "No such symbol"
+          redirect_to my_portfolio_path
+        end
+    else
+      flash[:danger] = "Search bar is empty"
+      redirect_to my_portfolio_path
+    end
+    end
 
 
 end
