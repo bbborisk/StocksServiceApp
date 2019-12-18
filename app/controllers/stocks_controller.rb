1eb7ca50df1b149c95f353fa8c3cf93af20bc556
bbborisk <bbborisk@gmail.com>
2019-12-18 10:22:04 +0100

    Error display fixed and refactored code

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,20 +1,15 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-        @stock = Stock.new_from_lookup(params[:stock])
-        if @stock
-          respond_to do |format|
-            format.js { render partial: 'users/result' }
-          end
-        else
-          flash[:danger] = "No such symbol"
-          redirect_to my_portfolio_path
-        end
-    else
-      flash[:danger] = "Search bar is empty"
-      redirect_to my_portfolio_path
-    end
-    end
 
+    if params[:stock].blank?
+      flash.now[:danger] = "Search bar is empty" #treba .now jer flash traje do refresha, a mi preko AJAXa renderujemo samo partial
+    else
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "No such symbol" unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
+    end
+  end
 
 end
