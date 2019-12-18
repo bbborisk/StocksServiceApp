fdf6d91e8a2ebc0d7b85d0e8b96e01977a9447d5
bbborisk <bbborisk@gmail.com>
2019-12-18 15:12:24 +0100

    Added friends listing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   resources :user_stocks, only:[:create, :destroy]
+  resources :users, only:[:show]
+  resources :friendships
   get 'my_friends', to: "users#my_friends"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
